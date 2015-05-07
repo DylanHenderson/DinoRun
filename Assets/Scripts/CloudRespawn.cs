86c8405870d31b4f46d497bf3180333486823f4b
Dylan <Dylan9h@gmail.com>
2015-05-07 18:53:26 +0200

    added moving background scrypt + background, configured jumping, configured lighting

---
+++ Assets/Scripts/CloudRespawn.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CloudRespawn : MonoBehaviour {
+	public float death_point;
+	public float respawn_point;
+
+	private float original_height;
+	// Use this for initialization
+	void Start () {
+		original_height = transform.position.y;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (transform.position.x < death_point) {
+			transform.position = new Vector3(respawn_point,original_height,transform.position.z);
+			transform.rotation = Quaternion.identity;
+		}
+	}
+}
---
+++ Assets/Scripts/MoveBackground.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MoveBackground : MonoBehaviour {
+
+
+	public float move_speed = 5;
+	public float time_to_increase = 10;
+	public float speed_increase = 1;
+
+	//how much we must offset the respawn by, could probably use skybox length for this.
+	public float offset;
+
+	public Transform target;
+	public Transform second_sky;
+
+	private Vector3 spawn_position;
+	// Use this for initialization
+	void Start () {
+		InvokeRepeating ("increaseSpeed",time_to_increase,time_to_increase);
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+
+		if (transform.position.x <= target.position.x) {
+			spawn_position = new Vector3 (second_sky.position.x+offset, transform.position.y, transform.position.z);
+			transform.position = spawn_position;
+		}
+
+		float step = move_speed/10.0f * Time.deltaTime;
+		transform.position = Vector3.MoveTowards(transform.position, target.position, step);
+	}
+
+
+	void increaseSpeed(){
+		move_speed += speed_increase;
+	}
+}
