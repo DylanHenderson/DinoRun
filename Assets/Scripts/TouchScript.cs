369fe7a4b4b84535821039c7e7f68f45ed0aa880
Zac Melnick <zacmelnick@gmail.com>
2015-05-14 23:35:50 +0200

    Added editor/mobile touch and simple pause button

--- Assets/Scripts/TouchScript.cs
+++ Assets/Scripts/TouchScript.cs
@@ -8,68 +8,181 @@ public class TouchScript : MonoBehaviour {
 	public Transform player;
 	public Transform planet;
 	public float speed =100;
-	public bool tapAction = false;
 	public float minSwipeLength = 200f;
 	Vector2 firstPressPos;
 	Vector2 secondPressPos;
 	Vector2 currentSwipe;
 	public static Swipe swipeDirection;
 
+	private RaycastHit hit;
+	private Ray ray;
+	public Camera titleCamera;
+	
 	// Use this for initialization
 	void Start () {
 	}
 	
 	// Update is called once per frame
-	void Update () {
+	void Update () {	
 
-		// If right charge, can be used for all special effects
-		if(swipeDirection == Swipe.Right)
-		{
-			Debug.Log ("Swipe Right");
-			swipeDirection = Swipe.None;
+		//Check if we are running either in the Unity editor or in a standalone build.
+		#if UNITY_STANDALONE || UNITY_WEBPLAYER
+		editorClick ();
+
+		//Check if we are running on iOS, Android, Windows Phone 8 or Unity iPhone
+		#elif UNITY_IOS || UNITY_ANDROID || UNITY_IPHONE
+		mobileTouch ();
+
+		#endif
+	}
+
+	void editorClick()
+	{
+		if (Input.GetMouseButtonDown(0) ) {
+
+			ray = titleCamera.ScreenPointToRay (Input.mousePosition);
+
+			if(Physics.Raycast (ray.origin, ray.direction, out hit))
+			{
+				switch(hit.collider.name){
+				case "Swipe Area":
+					firstPressPos = new Vector2(Input.mousePosition.x, Input.mousePosition.y);
+					break;
+				case "Tap Area":	
+					player.GetComponent<Rigidbody2D>().AddForce((planet.position - player.position).normalized * speed *-1);
+					break;
+				case "Pause":
+
+					if(Time.timeScale == 0)
+					{
+						Time.timeScale = 1;
+					}else
+					{
+						Time.timeScale = 0;
+					}
+
+					break;
+				}
+			}
+		}
+			
+		if (Input.GetMouseButtonUp (0)) {
+				
+			ray = titleCamera.ScreenPointToRay (Input.mousePosition);
+
+			if (Physics.Raycast (ray.origin, ray.direction, out hit)) {
+				switch (hit.collider.name) {
+				case "Swipe Area":
+					
+					secondPressPos = new Vector2 (Input.mousePosition.x, Input.mousePosition.y);
+					currentSwipe = new Vector3 (secondPressPos.x - firstPressPos.x, secondPressPos.y - firstPressPos.y);
+					
+					// Make sure it was a legit swipe, not a tap
+					if (currentSwipe.magnitude < minSwipeLength) {
+						swipeDirection = Swipe.None;
+						return;
+					}
+					
+					currentSwipe.Normalize ();
+					
+					// Swipe up
+					if (currentSwipe.y > 0 && currentSwipe.x > -0.5f && currentSwipe.x < 0.5f) {
+						//swipeDirection = Swipe.Up;
+						Debug.Log ("Swipe Up");
+						// Swipe down
+					} else if (currentSwipe.y < 0 && currentSwipe.x > -0.5f && currentSwipe.x < 0.5f) {
+						//swipeDirection = Swipe.Down;
+						Debug.Log ("Swipe Down");
+						// Swipe left
+					} else if (currentSwipe.x < 0 && currentSwipe.y > -0.5f && currentSwipe.y < 0.5f) {
+						//swipeDirection = Swipe.Left;
+						Debug.Log ("Swipe Left");
+						// Swipe right
+					} else if (currentSwipe.x > 0 && currentSwipe.y > -0.5f && currentSwipe.y < 0.5f) {
+						//swipeDirection = Swipe.Right;
+						
+						Debug.Log ("Swipe Right");
+					}
+					
+					break;
+				}
+				
+			}
 		}
 	}
 
-	void OnMouseDown() 
+	void mobileTouch()
 	{
-		if(tapAction)
-		{
-			player.GetComponent<Rigidbody2D>().AddForce((planet.position - player.position).normalized * speed *-1);
-		}else{
+		if (Input.touches.Length > 0) {
+			Touch touch = Input.touches [0];
+			ray = titleCamera.ScreenPointToRay (Input.touches [0].position);
+			
+			if (touch.phase == TouchPhase.Began && Physics.Raycast (ray.origin, ray.direction, out hit)) {
+				
+				switch(hit.collider.name){
+				case "Swipe Area":
+					firstPressPos = new Vector2(touch.position.x, touch.position.y);
+					break;
+				case "Tap Area":
+					player.GetComponent<Rigidbody2D>().AddForce((planet.position - player.position).normalized * speed *-1);
+					break;
+				case "Pause Button":
 
-			firstPressPos = new Vector2(Input.mousePosition.x,Input.mousePosition.y);
+					if(Time.timeScale == 0)
+					{
+						Time.timeScale = 1;
+					}else
+					{
+						Time.timeScale = 0;
+					}
+
+					break;
+				}
+			}
+			
+			if (touch.phase == TouchPhase.Ended  && Physics.Raycast (ray.origin, ray.direction, out hit)) {
+				
+				
+				switch(hit.collider.name){
+				case "Swipe Area":
+					
+					secondPressPos = new Vector2(touch.position.x, touch.position.y);
+					currentSwipe = new Vector3(secondPressPos.x - firstPressPos.x, secondPressPos.y - firstPressPos.y);
+					
+					// Make sure it was a legit swipe, not a tap
+					if (currentSwipe.magnitude < minSwipeLength) {
+						swipeDirection = Swipe.None;
+						return;
+					}
+					
+					currentSwipe.Normalize();
+					
+					// Swipe up
+					if (currentSwipe.y > 0 && currentSwipe.x > -0.5f && currentSwipe.x < 0.5f) {
+						//swipeDirection = Swipe.Up;
+						Debug.Log ("Swipe Up");
+						// Swipe down
+					} else if (currentSwipe.y < 0 && currentSwipe.x > -0.5f && currentSwipe.x < 0.5f) {
+						//swipeDirection = Swipe.Down;
+						Debug.Log ("Swipe Down");
+						// Swipe left
+					} else if (currentSwipe.x < 0 && currentSwipe.y > -0.5f && currentSwipe.y < 0.5f) {
+						//swipeDirection = Swipe.Left;
+						Debug.Log ("Swipe Left");
+						// Swipe right
+					} else if (currentSwipe.x > 0 && currentSwipe.y > -0.5f && currentSwipe.y < 0.5f) {
+						//swipeDirection = Swipe.Right;
+						
+						Debug.Log ("Swipe Right");
+					}
+					
+					break;
+				}
+				
+			}
+		} else if(swipeDirection != Swipe.None) {
+			swipeDirection = Swipe.None;   
 		}
 	}
-
-	void OnMouseUp()
-	{
 		
-		if (!tapAction) {
-			secondPressPos = new Vector2(Input.mousePosition.x,Input.mousePosition.y);
-			currentSwipe = new Vector3(secondPressPos.x - firstPressPos.x, secondPressPos.y - firstPressPos.y);
-
-			// Make sure it was a legit swipe, not a tap
-			if (currentSwipe.magnitude < minSwipeLength) {
-				swipeDirection = Swipe.None;
-				return;
-			}
-
-			currentSwipe.Normalize();
-			
-			// Swipe up
-			if (currentSwipe.y > 0 && currentSwipe.x > -0.5f && currentSwipe.x < 0.5f) {
-				swipeDirection = Swipe.Up;
-				// Swipe down
-			} else if (currentSwipe.y < 0 && currentSwipe.x > -0.5f && currentSwipe.x < 0.5f) {
-				swipeDirection = Swipe.Down;
-				// Swipe left
-			} else if (currentSwipe.x < 0 && currentSwipe.y > -0.5f && currentSwipe.y < 0.5f) {
-				swipeDirection = Swipe.Left;
-				// Swipe right
-			} else if (currentSwipe.x > 0 && currentSwipe.y > -0.5f && currentSwipe.y < 0.5f) {
-				swipeDirection = Swipe.Right;
-			}
-			
-		}
-	}
 }
