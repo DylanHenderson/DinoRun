a93bc33caece71b5a62c0ecd914363f8c64ca9cb
Dylan <Dylan9h@gmail.com>
2015-05-14 20:01:09 +0200

    added power up bar, fills over time

---
+++ Assets/Scripts/PowerUpBar.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class PowerUpBar : MonoBehaviour {
+
+	Image myImage;
+	// Use this for initialization
+	void Start () {
+		InvokeRepeating ("increasePower",0.5f,0.5f);
+		myImage = GetComponent<Image>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void increasePower(){
+
+
+		if (myImage.fillAmount >= 360) {
+			//ready for powerup
+		} else {
+			myImage.fillAmount += 0.01f;
+		}
+
+	}
+}
