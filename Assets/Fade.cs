53d1258fd0c09c3bbaef3d4e731a4bf62f372302
Zac Melnick <zacmelnick@gmail.com>
2015-05-15 23:38:54 +0200

    Added fade in to scene

---
+++ Assets/Fade.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Fade : MonoBehaviour {
+
+	public Texture2D fadeTexture;
+	public float fadeSpeed = 0.2f;
+	public int drawDepth = -1000;
+	
+	private float alpha = 1.0f; 
+	private float fadeDir = -1f;
+
+	// Use this for initialization
+	void Start () {
+	}
+	
+	void OnGUI(){ 
+
+		if (alpha > 0f) {
+				alpha += fadeDir * fadeSpeed * Time.deltaTime; 
+				alpha = Mathf.Clamp01 (alpha); 
+
+				// GUI.color = new Color() { a = 0.5f }; 
+				GUI.color = new Color () {a=alpha};
+				GUI.depth = drawDepth; 
+				GUI.DrawTexture (new Rect (0, 0, Screen.width, Screen.height), fadeTexture); 
+			} 
+	}
+}
