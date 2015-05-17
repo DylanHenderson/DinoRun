c8d7522ac72b4a17c66a584e2d05d40273eb96f1
Dylan <Dylan9h@gmail.com>
2015-05-17 12:51:08 +0200

    remodeled power bar, added pterodactyl movement, added to player controler script

--- Assets/Scripts/ComputerJumpScript.cs
+++ Assets/Scripts/ComputerJumpScript.cs
@@ -8,9 +8,11 @@ public class ComputerJumpScript : MonoBehaviour {
 	public float speed =100;
 	public float gravity = 1;
 	public Transform planet;
-
+	public GameObject bird;
 
 	public GameObject power_bar;
+	public Transform start_point;
+	public float bird_speed;
 
 
 	// Use this for initialization
@@ -19,6 +21,9 @@ public class ComputerJumpScript : MonoBehaviour {
 	private float original_gravity;
 	private float original_height;
 	private PowerUpBar pb;
+	private Vector3 end_point;
+	private float original_bird_x;
+	private float current_dino_x;
 
 
 	void Start () {
@@ -29,6 +34,8 @@ public class ComputerJumpScript : MonoBehaviour {
 		//power_bar.GetComponent<PowerUpBar>().
 
 		pb = power_bar.GetComponent<PowerUpBar> ();
+		original_bird_x = bird.transform.position.x;
+		current_dino_x = gameObject.transform.position.x;
 
 	}
 	
@@ -46,19 +53,26 @@ public class ComputerJumpScript : MonoBehaviour {
 
 		if (flying && pb.getcanFly ()) {
 
-			gameObject.transform.position = new Vector2 (gameObject.transform.position.x, original_height + 1);
+			gameObject.transform.position = new Vector2 (current_dino_x, original_height + 1);
+			end_point = new Vector3(gameObject.transform.position.x,bird.transform.position.y,bird.transform.position.z);
+
+
+
+			bird.transform.position = Vector3.Lerp(start_point.position,end_point,bird_speed);
+
+
 		} else {
 			flying = false;
 			gravity = original_gravity;
 			pb.cancelDecrease();
-			
+			bird.transform.position = new Vector2 (original_bird_x, bird.transform.position.y);
 		}
 
 		if (Input.GetKeyDown ("x") && pb.getcanFly ()){
 			flying = true;
 			gravity = 0;
 			pb.initiateDecrease();
-
+			current_dino_x = gameObject.transform.position.x;
 
 
 		}
@@ -67,7 +81,7 @@ public class ComputerJumpScript : MonoBehaviour {
 			flying = false;
 			gravity = original_gravity;
 			pb.cancelDecrease();
-
+			bird.transform.position = new Vector2 (original_bird_x, bird.transform.position.y);
 			
 		}
 	}
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -3,22 +3,42 @@ using System.Collections;
 
 public class PlayerController : MonoBehaviour {
 
-	private Animator animator;
-	private Rigidbody2D r2;
+
 	public Transform planet;
 	public Sprite jump;
 	public Sprite defaultSprite;
-
+	public GameObject bird;
+	public GameObject power_bar;
+	public Transform start_point;
+	public float bird_speed;
 	public float speed = 100;
 	public float gravity = 1;
-	private float worldSpeed = 0;
 	public bool grounded = false;
 
-	// Use this for initialization
-	void Start()
-	{
+
+	private Animator animator;
+	private Rigidbody2D r2;
+	private float worldSpeed = 0;
+	private bool flying;
+	private float original_gravity;
+	private float original_height;
+	private PowerUpBar pb;
+	private Vector3 end_point;
+	private float original_bird_x;
+	private float current_dino_x;
+	
+	void Start () {
 		animator = this.GetComponent<Animator>();
 		r2 = GetComponent<Rigidbody2D>();
+		flying = false;
+		original_gravity = gravity;
+		original_height = gameObject.transform.position.y;
+		//power_bar.GetComponent<PowerUpBar>().
+		
+		pb = power_bar.GetComponent<PowerUpBar> ();
+		original_bird_x = bird.transform.position.x;
+		current_dino_x = gameObject.transform.position.x;
+		
 	}
 	
 	// Update is called once per frame
@@ -31,6 +51,44 @@ public class PlayerController : MonoBehaviour {
 		if (Input.GetKeyDown ("space") && grounded) {
 			r2.AddForce((planet.position - transform.position).normalized * speed *-1);
 		}
+
+		//when player is flying and still has power left
+		if (flying && pb.getcanFly ()) {
+			
+			gameObject.transform.position = new Vector2 (current_dino_x, original_height + 1);
+			end_point = new Vector3(gameObject.transform.position.x,bird.transform.position.y,bird.transform.position.z);
+			
+			
+			
+			bird.transform.position = Vector3.Lerp(start_point.position,end_point,bird_speed);
+			
+		//can no longer fly push to ground, send bird back to position	
+		} else {
+			flying = false;
+			gravity = original_gravity;
+			pb.cancelDecrease();
+			bird.transform.position = new Vector2 (original_bird_x, bird.transform.position.y);
+		}
+
+		//initiate flying
+		if (Input.GetKeyDown ("x") && pb.getcanFly ()){
+			flying = true;
+			gravity = 0;
+			pb.initiateDecrease();
+			current_dino_x = gameObject.transform.position.x;
+			
+			
+		}
+
+		//no longer flying
+		if (Input.GetKeyUp ("x")) {
+			flying = false;
+			gravity = original_gravity;
+			pb.cancelDecrease();
+			bird.transform.position = new Vector2 (original_bird_x, bird.transform.position.y);
+			
+		}
+
 
 		// Cancel animation while jumping
 		if(transform.position.y < 1.5f && !grounded) {
@@ -63,6 +121,8 @@ public class PlayerController : MonoBehaviour {
 			}
 		}
 	}
+
+
 
 	/*
 	 * Normalize number between 0 and 1
