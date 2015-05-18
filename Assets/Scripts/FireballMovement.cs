600a6a44a8290553a3e17dd45d607a12bb35ec93
Dylan <Dylan9h@gmail.com>
2015-05-19 01:07:10 +0200

    fireball change

--- Assets/Scripts/FireballMovement.cs
+++ Assets/Scripts/FireballMovement.cs
@@ -4,16 +4,30 @@ using System.Collections;
 public class FireballMovement : MonoBehaviour {
 
 	public Transform target_crash;
+	public GameObject explosion;
 	public float speed = 10;
 	// Use this for initialization
 	void Start () {
+
 		Destroy (gameObject,1);
+		Invoke ("explode",2);
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		float step = speed * Time.deltaTime;
 		transform.position = Vector3.Lerp(transform.position,target_crash.position,speed);
+
+		GameObject swoosh = Instantiate(explosion,explosion.transform.position,Quaternion.identity) as GameObject;
+		Destroy (swoosh, 4);
+		
+		
 	}
 
+	void explode(){
+		GameObject swoosh = Instantiate(explosion,explosion.transform.position,Quaternion.identity) as GameObject;
+		Destroy (swoosh, 4);
+		
+	}
+	
 }
